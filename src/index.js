fdc3be4ecfa3a764e9f1eca93a1d44cb38283f9a
Leonardo Migliorelli <56474420+Glydric@users.noreply.github.com>
2023-07-18 17:11:36 +0200

    Improvements as the user might effectly use other paths in other contexts

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ const path = require("path")
 
 export function baseUrl(base) {
     base = base.trim().replace(/\/+$/, '/')
-
-    if (!base.startsWith('/')) { 
-        console.error("BaseUrl should begin from /")
-        return
-    }
-
+    
     return {
-        walkTokens(token) {
-          // TODO take in mind that maybe link work better with standard implementation, might be need to distinguish they
-            if (!['link', 'image'].includes(token.type)) { return; }
+      walkTokens(token) {
+        // TODO take in mind that maybe link work better with standard implementation, might be need to distinguish they
+        if (!['link', 'image'].includes(token.type)) { return; }
             
             // the URL is already from root
             if (token.href.startsWith('/')) { return; }
